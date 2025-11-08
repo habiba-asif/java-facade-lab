6b961285be571a1f76747bbbfa8e9cb734db6509
habiba-asif <haasif1@empirictech.com>
2025-11-08 14:17:37 +0000

    first commit

---
+++ src/lab/facade/Memory.java
@@ -0,0 +1,7 @@
+package lab.facade;
+
+public class Memory {
+    public void load(long position, byte[] data) {
+        System.out.println(System.currentTimeMillis() + " | Memory: loading " + data.length + " bytes at " + position);
+    }
+}
